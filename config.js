df51d4b9d6d0cc35fe66d2c751ea48420005922f
Praveen-AHTPL <Praveen.Ramu@analytixhub.ai>
2025-11-26 16:43:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // public/config.js
 window.env = {
-  API_URL: "https://react-backend-data-and-analytics-practice.onrender.com"
+  API_URL: "https://react-backend-gmavguezdpaqf9an.centralindia-01.azurewebsites.net"
 };
